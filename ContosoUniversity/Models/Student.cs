fe789fd629d8dcc23353d0c7e7b8f77dd3fec675
Edin Jašarević <edinjasar14@gmail.com>
2018-08-03 21:37:20 +0200

    full name

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -11,17 +11,28 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
-        [StringLength(50)]
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-
         [Column("FirstName")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
